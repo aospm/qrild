9d96fecd7ff15704c155c9b020739abe7b383183
Caleb Connolly <caleb.connolly@linaro.org>
2022-07-25 01:07:49 +0100

    _android: qmi_uim: get_slot_status

--- _android/qmi_uim.c
+++ _android/qmi_uim.c
@@ -277,3 +277,355 @@ void uim_change_provisioning_session_resp_free(struct uim_change_provisioning_se
 	qmi_tlv_free((struct qmi_tlv*)change_provisioning_session_resp);
 }
 
+struct uim_get_slot_status_req *uim_get_slot_status_req_alloc(unsigned txn)
+{
+	return (struct uim_get_slot_status_req*)qmi_tlv_init(txn, 71, 0);
+}
+
+struct uim_get_slot_status_req *uim_get_slot_status_req_parse(void *buf, size_t len)
+{
+	return (struct uim_get_slot_status_req*)qmi_tlv_decode(buf, len);
+}
+
+void *uim_get_slot_status_req_encode(struct uim_get_slot_status_req *get_slot_status_req, size_t *len)
+{
+	return qmi_tlv_encode((struct qmi_tlv*)get_slot_status_req, len);
+}
+
+void uim_get_slot_status_req_free(struct uim_get_slot_status_req *get_slot_status_req)
+{
+	qmi_tlv_free((struct qmi_tlv*)get_slot_status_req);
+}
+
+struct uim_get_slot_status_resp *uim_get_slot_status_resp_alloc(unsigned txn)
+{
+	return (struct uim_get_slot_status_resp*)qmi_tlv_init(txn, 71, 2);
+}
+
+struct uim_get_slot_status_resp *uim_get_slot_status_resp_parse(void *buf, size_t len)
+{
+	return (struct uim_get_slot_status_resp*)qmi_tlv_decode(buf, len);
+}
+
+void *uim_get_slot_status_resp_encode(struct uim_get_slot_status_resp *get_slot_status_resp, size_t *len)
+{
+	return qmi_tlv_encode((struct qmi_tlv*)get_slot_status_resp, len);
+}
+
+void uim_get_slot_status_resp_free(struct uim_get_slot_status_resp *get_slot_status_resp)
+{
+	qmi_tlv_free((struct qmi_tlv*)get_slot_status_resp);
+}
+
+int uim_get_slot_status_resp_set_slot_state(struct uim_get_slot_status_resp *get_slot_status_resp, struct uim_physical_slot_state *val)
+{
+	size_t len = 0;
+	int rc;
+	// FIXME: use realloc dynamically instead
+	void *ptr = malloc(1024);
+	memset(ptr, 0, 1024);
+	*((uint8_t*)(ptr + len)) = val->slots_n;
+	len += 1;
+	for(size_t i = 0; i < val->slots_n; i++) {
+		*((uint32_t*)(ptr + len)) = val->slots[i].card_state;
+		len += 4;
+		*((uint32_t*)(ptr + len)) = val->slots[i].slot_state;
+		len += 4;
+		*((uint8_t*)(ptr + len)) = val->slots[i].logical_slot;
+		len += 1;
+		*((uint8_t*)(ptr + len)) = val->slots[i].iccid_n;
+		len += 1;
+		for(size_t ii = 0; ii < val->slots[i].iccid_n; ii++) {
+			*((uint8_t*)(ptr + len)) = val->slots[i].iccid[ii];
+			len += 1;
+		}
+	}
+	rc = qmi_tlv_set((struct qmi_tlv*)get_slot_status_resp, 16, ptr, len);
+	free(ptr);
+	return rc;
+}
+
+struct uim_physical_slot_state *uim_get_slot_status_resp_get_slot_state(struct uim_get_slot_status_resp *get_slot_status_resp)
+{
+	size_t len = 0, buf_sz;
+	uint8_t *ptr;
+	struct uim_physical_slot_state *out;
+
+	ptr = qmi_tlv_get((struct qmi_tlv*)get_slot_status_resp, 16, &buf_sz);
+	if (!ptr)
+		return NULL;
+
+	out = malloc(sizeof(struct uim_physical_slot_state));
+	out->slots_n = get_next(uint8_t, 1);
+	size_t slots_sz = sizeof(struct physical_slot_state_slots);
+	out->slots = malloc(slots_sz * out->slots_n);
+	for(size_t i = 0; i < out->slots_n; i++) {
+		out->slots[i].card_state = get_next(uint32_t, 4);
+		out->slots[i].slot_state = get_next(uint32_t, 4);
+		out->slots[i].logical_slot = get_next(uint8_t, 1);
+		out->slots[i].iccid_n = get_next(uint8_t, 1);
+		size_t iccid_sz = 1;
+		out->slots[i].iccid = malloc(iccid_sz * out->slots[i].iccid_n);
+		for(size_t ii = 0; ii < out->slots[i].iccid_n; ii++) {
+			out->slots[i].iccid[ii] = get_next(uint8_t, 1);
+		}
+	}
+
+	return out;
+
+err_wrong_len:
+	printf("%s: expected at least %zu bytes but got %zu\n", __func__, len, buf_sz);
+	free(out);
+	return NULL;
+}
+
+int uim_get_slot_status_resp_set_slot_info(struct uim_get_slot_status_resp *get_slot_status_resp, struct uim_physical_slot_info *val)
+{
+	size_t len = 0;
+	int rc;
+	// FIXME: use realloc dynamically instead
+	void *ptr = malloc(1024);
+	memset(ptr, 0, 1024);
+	*((uint8_t*)(ptr + len)) = val->slots_n;
+	len += 1;
+	for(size_t i = 0; i < val->slots_n; i++) {
+		*((uint32_t*)(ptr + len)) = val->slots[i].card_protocol;
+		len += 4;
+		*((uint8_t*)(ptr + len)) = val->slots[i].valid_applications;
+		len += 1;
+		*((uint8_t*)(ptr + len)) = val->slots[i].atr_value_n;
+		len += 1;
+		for(size_t ii = 0; ii < val->slots[i].atr_value_n; ii++) {
+			*((uint8_t*)(ptr + len)) = val->slots[i].atr_value[ii];
+			len += 1;
+		}
+		*((uint8_t*)(ptr + len)) = val->slots[i].is_euicc;
+		len += 1;
+	}
+	rc = qmi_tlv_set((struct qmi_tlv*)get_slot_status_resp, 17, ptr, len);
+	free(ptr);
+	return rc;
+}
+
+struct uim_physical_slot_info *uim_get_slot_status_resp_get_slot_info(struct uim_get_slot_status_resp *get_slot_status_resp)
+{
+	size_t len = 0, buf_sz;
+	uint8_t *ptr;
+	struct uim_physical_slot_info *out;
+
+	ptr = qmi_tlv_get((struct qmi_tlv*)get_slot_status_resp, 17, &buf_sz);
+	if (!ptr)
+		return NULL;
+
+	out = malloc(sizeof(struct uim_physical_slot_info));
+	out->slots_n = get_next(uint8_t, 1);
+	size_t slots_sz = sizeof(struct physical_slot_info_slots);
+	out->slots = malloc(slots_sz * out->slots_n);
+	for(size_t i = 0; i < out->slots_n; i++) {
+		out->slots[i].card_protocol = get_next(uint32_t, 4);
+		out->slots[i].valid_applications = get_next(uint8_t, 1);
+		out->slots[i].atr_value_n = get_next(uint8_t, 1);
+		size_t atr_value_sz = 1;
+		out->slots[i].atr_value = malloc(atr_value_sz * out->slots[i].atr_value_n);
+		for(size_t ii = 0; ii < out->slots[i].atr_value_n; ii++) {
+			out->slots[i].atr_value[ii] = get_next(uint8_t, 1);
+		}
+		out->slots[i].is_euicc = get_next(uint8_t, 1);
+	}
+
+	return out;
+
+err_wrong_len:
+	printf("%s: expected at least %zu bytes but got %zu\n", __func__, len, buf_sz);
+	free(out);
+	return NULL;
+}
+
+int uim_get_slot_status_resp_set_eid_info(struct uim_get_slot_status_resp *get_slot_status_resp, uint8_t *val, size_t count)
+{
+	return qmi_tlv_set_array((struct qmi_tlv*)get_slot_status_resp, 18, 1, val, count, sizeof(uint8_t));
+}
+
+uint8_t *uim_get_slot_status_resp_get_eid_info(struct uim_get_slot_status_resp *get_slot_status_resp, size_t *count)
+{
+	uint8_t *ptr;
+	size_t size;
+	size_t len;
+
+	ptr = qmi_tlv_get_array((struct qmi_tlv*)get_slot_status_resp, 18, 1, &len, &size);
+	if (!ptr)
+		return NULL;
+
+	if (size != sizeof(uint8_t))
+		return NULL;
+
+	*count = len;
+	return ptr;
+}
+
+struct uim_get_slot_status_ind *uim_get_slot_status_ind_alloc(unsigned txn)
+{
+	return (struct uim_get_slot_status_ind*)qmi_tlv_init(txn, 72, 4);
+}
+
+struct uim_get_slot_status_ind *uim_get_slot_status_ind_parse(void *buf, size_t len)
+{
+	return (struct uim_get_slot_status_ind*)qmi_tlv_decode(buf, len);
+}
+
+void *uim_get_slot_status_ind_encode(struct uim_get_slot_status_ind *get_slot_status_ind, size_t *len)
+{
+	return qmi_tlv_encode((struct qmi_tlv*)get_slot_status_ind, len);
+}
+
+void uim_get_slot_status_ind_free(struct uim_get_slot_status_ind *get_slot_status_ind)
+{
+	qmi_tlv_free((struct qmi_tlv*)get_slot_status_ind);
+}
+
+int uim_get_slot_status_ind_set_slot_state(struct uim_get_slot_status_ind *get_slot_status_ind, struct uim_physical_slot_state *val)
+{
+	size_t len = 0;
+	int rc;
+	// FIXME: use realloc dynamically instead
+	void *ptr = malloc(1024);
+	memset(ptr, 0, 1024);
+	*((uint8_t*)(ptr + len)) = val->slots_n;
+	len += 1;
+	for(size_t i = 0; i < val->slots_n; i++) {
+		*((uint32_t*)(ptr + len)) = val->slots[i].card_state;
+		len += 4;
+		*((uint32_t*)(ptr + len)) = val->slots[i].slot_state;
+		len += 4;
+		*((uint8_t*)(ptr + len)) = val->slots[i].logical_slot;
+		len += 1;
+		*((uint8_t*)(ptr + len)) = val->slots[i].iccid_n;
+		len += 1;
+		for(size_t ii = 0; ii < val->slots[i].iccid_n; ii++) {
+			*((uint8_t*)(ptr + len)) = val->slots[i].iccid[ii];
+			len += 1;
+		}
+	}
+	rc = qmi_tlv_set((struct qmi_tlv*)get_slot_status_ind, 16, ptr, len);
+	free(ptr);
+	return rc;
+}
+
+struct uim_physical_slot_state *uim_get_slot_status_ind_get_slot_state(struct uim_get_slot_status_ind *get_slot_status_ind)
+{
+	size_t len = 0, buf_sz;
+	uint8_t *ptr;
+	struct uim_physical_slot_state *out;
+
+	ptr = qmi_tlv_get((struct qmi_tlv*)get_slot_status_ind, 16, &buf_sz);
+	if (!ptr)
+		return NULL;
+
+	out = malloc(sizeof(struct uim_physical_slot_state));
+	out->slots_n = get_next(uint8_t, 1);
+	size_t slots_sz = sizeof(struct physical_slot_state_slots);
+	out->slots = malloc(slots_sz * out->slots_n);
+	for(size_t i = 0; i < out->slots_n; i++) {
+		out->slots[i].card_state = get_next(uint32_t, 4);
+		out->slots[i].slot_state = get_next(uint32_t, 4);
+		out->slots[i].logical_slot = get_next(uint8_t, 1);
+		out->slots[i].iccid_n = get_next(uint8_t, 1);
+		size_t iccid_sz = 1;
+		out->slots[i].iccid = malloc(iccid_sz * out->slots[i].iccid_n);
+		for(size_t ii = 0; ii < out->slots[i].iccid_n; ii++) {
+			out->slots[i].iccid[ii] = get_next(uint8_t, 1);
+		}
+	}
+
+	return out;
+
+err_wrong_len:
+	printf("%s: expected at least %zu bytes but got %zu\n", __func__, len, buf_sz);
+	free(out);
+	return NULL;
+}
+
+int uim_get_slot_status_ind_set_slot_info(struct uim_get_slot_status_ind *get_slot_status_ind, struct uim_physical_slot_info *val)
+{
+	size_t len = 0;
+	int rc;
+	// FIXME: use realloc dynamically instead
+	void *ptr = malloc(1024);
+	memset(ptr, 0, 1024);
+	*((uint8_t*)(ptr + len)) = val->slots_n;
+	len += 1;
+	for(size_t i = 0; i < val->slots_n; i++) {
+		*((uint32_t*)(ptr + len)) = val->slots[i].card_protocol;
+		len += 4;
+		*((uint8_t*)(ptr + len)) = val->slots[i].valid_applications;
+		len += 1;
+		*((uint8_t*)(ptr + len)) = val->slots[i].atr_value_n;
+		len += 1;
+		for(size_t ii = 0; ii < val->slots[i].atr_value_n; ii++) {
+			*((uint8_t*)(ptr + len)) = val->slots[i].atr_value[ii];
+			len += 1;
+		}
+		*((uint8_t*)(ptr + len)) = val->slots[i].is_euicc;
+		len += 1;
+	}
+	rc = qmi_tlv_set((struct qmi_tlv*)get_slot_status_ind, 17, ptr, len);
+	free(ptr);
+	return rc;
+}
+
+struct uim_physical_slot_info *uim_get_slot_status_ind_get_slot_info(struct uim_get_slot_status_ind *get_slot_status_ind)
+{
+	size_t len = 0, buf_sz;
+	uint8_t *ptr;
+	struct uim_physical_slot_info *out;
+
+	ptr = qmi_tlv_get((struct qmi_tlv*)get_slot_status_ind, 17, &buf_sz);
+	if (!ptr)
+		return NULL;
+
+	out = malloc(sizeof(struct uim_physical_slot_info));
+	out->slots_n = get_next(uint8_t, 1);
+	size_t slots_sz = sizeof(struct physical_slot_info_slots);
+	out->slots = malloc(slots_sz * out->slots_n);
+	for(size_t i = 0; i < out->slots_n; i++) {
+		out->slots[i].card_protocol = get_next(uint32_t, 4);
+		out->slots[i].valid_applications = get_next(uint8_t, 1);
+		out->slots[i].atr_value_n = get_next(uint8_t, 1);
+		size_t atr_value_sz = 1;
+		out->slots[i].atr_value = malloc(atr_value_sz * out->slots[i].atr_value_n);
+		for(size_t ii = 0; ii < out->slots[i].atr_value_n; ii++) {
+			out->slots[i].atr_value[ii] = get_next(uint8_t, 1);
+		}
+		out->slots[i].is_euicc = get_next(uint8_t, 1);
+	}
+
+	return out;
+
+err_wrong_len:
+	printf("%s: expected at least %zu bytes but got %zu\n", __func__, len, buf_sz);
+	free(out);
+	return NULL;
+}
+
+int uim_get_slot_status_ind_set_eid_info(struct uim_get_slot_status_ind *get_slot_status_ind, uint8_t *val, size_t count)
+{
+	return qmi_tlv_set_array((struct qmi_tlv*)get_slot_status_ind, 18, 1, val, count, sizeof(uint8_t));
+}
+
+uint8_t *uim_get_slot_status_ind_get_eid_info(struct uim_get_slot_status_ind *get_slot_status_ind, size_t *count)
+{
+	uint8_t *ptr;
+	size_t size;
+	size_t len;
+
+	ptr = qmi_tlv_get_array((struct qmi_tlv*)get_slot_status_ind, 18, 1, &len, &size);
+	if (!ptr)
+		return NULL;
+
+	if (size != sizeof(uint8_t))
+		return NULL;
+
+	*count = len;
+	return ptr;
+}
+
--- _android/qmi_uim.h
+++ _android/qmi_uim.h
@@ -17,7 +17,13 @@
 
 #define QMI_UIM_GET_CARD_STATUS 47
 #define QMI_UIM_CHANGE_PROVISIONING_SESSION 56
+#define QMI_UIM_GET_SLOT_STATUS 71
+#define QMI_UIM_SLOT_STATUS_IND 72
 #define QMI_UIM_SESSION_TYPE_PRIMARY_GW_PROVISIONING 0
+#define QMI_UIM_CARD_STATE_ABSENT 0
+#define QMI_UIM_CARD_STATE_PRESENT 1
+#define QMI_UIM_CARD_STATE_ERROR 2
+#define QMI_UIM_CARD_STATE_RESTRICTED 4
 
 struct uim_card_status {
 	uint16_t index_gw_primary;
@@ -63,10 +69,42 @@ struct uim_provisioning_session_application {
 	uint8_t *application_identifier_value;
 };
 
+struct uim_physical_slot_state {
+	uint8_t slots_n;
+	struct physical_slot_state_slots {
+		uint32_t card_state;
+		uint32_t slot_state;
+		uint8_t logical_slot;
+		uint8_t iccid_n;
+		uint8_t *iccid;
+	} *slots;
+};
+
+struct uim_physical_slot_info {
+	uint8_t slots_n;
+	struct physical_slot_info_slots {
+		uint32_t card_protocol;
+		uint8_t valid_applications;
+		uint8_t atr_value_n;
+		uint8_t *atr_value;
+		uint8_t is_euicc;
+	} *slots;
+};
+
+struct uim_slot_status_msg {
+	struct uim_physical_slot_state *state;
+	struct uim_physical_slot_info *info;
+	uint8_t eid_info_n;
+	uint8_t *eid_info;
+};
+
 struct uim_get_card_status_req;
 struct uim_get_card_status_resp;
 struct uim_change_provisioning_session_req;
 struct uim_change_provisioning_session_resp;
+struct uim_get_slot_status_req;
+struct uim_get_slot_status_resp;
+struct uim_get_slot_status_ind;
 
 /*
  * uim_get_card_status_req message
@@ -108,5 +146,47 @@ struct uim_change_provisioning_session_resp *uim_change_provisioning_session_res
 struct uim_change_provisioning_session_resp *uim_change_provisioning_session_resp_parse(void *buf, size_t len);
 void *uim_change_provisioning_session_resp_encode(struct uim_change_provisioning_session_resp *change_provisioning_session_resp, size_t *len);
 void uim_change_provisioning_session_resp_free(struct uim_change_provisioning_session_resp *change_provisioning_session_resp);
+
+/*
+ * uim_get_slot_status_req message
+ */
+struct uim_get_slot_status_req *uim_get_slot_status_req_alloc(unsigned txn);
+struct uim_get_slot_status_req *uim_get_slot_status_req_parse(void *buf, size_t len);
+void *uim_get_slot_status_req_encode(struct uim_get_slot_status_req *get_slot_status_req, size_t *len);
+void uim_get_slot_status_req_free(struct uim_get_slot_status_req *get_slot_status_req);
+
+/*
+ * uim_get_slot_status_resp message
+ */
+struct uim_get_slot_status_resp *uim_get_slot_status_resp_alloc(unsigned txn);
+struct uim_get_slot_status_resp *uim_get_slot_status_resp_parse(void *buf, size_t len);
+void *uim_get_slot_status_resp_encode(struct uim_get_slot_status_resp *get_slot_status_resp, size_t *len);
+void uim_get_slot_status_resp_free(struct uim_get_slot_status_resp *get_slot_status_resp);
+
+int uim_get_slot_status_resp_set_slot_state(struct uim_get_slot_status_resp *get_slot_status_resp, struct uim_physical_slot_state *val);
+struct uim_physical_slot_state *uim_get_slot_status_resp_get_slot_state(struct uim_get_slot_status_resp *get_slot_status_resp);
+
+int uim_get_slot_status_resp_set_slot_info(struct uim_get_slot_status_resp *get_slot_status_resp, struct uim_physical_slot_info *val);
+struct uim_physical_slot_info *uim_get_slot_status_resp_get_slot_info(struct uim_get_slot_status_resp *get_slot_status_resp);
+
+int uim_get_slot_status_resp_set_eid_info(struct uim_get_slot_status_resp *get_slot_status_resp, uint8_t *val, size_t count);
+uint8_t *uim_get_slot_status_resp_get_eid_info(struct uim_get_slot_status_resp *get_slot_status_resp, size_t *count);
+
+/*
+ * uim_get_slot_status_ind message
+ */
+struct uim_get_slot_status_ind *uim_get_slot_status_ind_alloc(unsigned txn);
+struct uim_get_slot_status_ind *uim_get_slot_status_ind_parse(void *buf, size_t len);
+void *uim_get_slot_status_ind_encode(struct uim_get_slot_status_ind *get_slot_status_ind, size_t *len);
+void uim_get_slot_status_ind_free(struct uim_get_slot_status_ind *get_slot_status_ind);
+
+int uim_get_slot_status_ind_set_slot_state(struct uim_get_slot_status_ind *get_slot_status_ind, struct uim_physical_slot_state *val);
+struct uim_physical_slot_state *uim_get_slot_status_ind_get_slot_state(struct uim_get_slot_status_ind *get_slot_status_ind);
+
+int uim_get_slot_status_ind_set_slot_info(struct uim_get_slot_status_ind *get_slot_status_ind, struct uim_physical_slot_info *val);
+struct uim_physical_slot_info *uim_get_slot_status_ind_get_slot_info(struct uim_get_slot_status_ind *get_slot_status_ind);
+
+int uim_get_slot_status_ind_set_eid_info(struct uim_get_slot_status_ind *get_slot_status_ind, uint8_t *val, size_t count);
+uint8_t *uim_get_slot_status_ind_get_eid_info(struct uim_get_slot_status_ind *get_slot_status_ind, size_t *count);
 
 #endif
